704ef8598d3913605dde6d0c61e2a8c731986f9d
채영준 <157681635+cyjoon68@users.noreply.github.com>
2026-08-29 00:56:27 +0900

    fix(mobile): remove invalid platform exports

--- packages/mobile/src/action-button-group/index.ts
+++ packages/mobile/src/action-button-group/index.ts
@@ -1,4 +1,2 @@
 export { default as ActionButtonGroup } from "./action-button-group";
-export { default as ActionButtonCircularPairGroup } from "./action-button-circular-pair-group";
-export { default as ActionButtonCapsuleGroup } from "./action-button-capsule-group";
 export * from "./action-button-group.types";
